64f6fa4dadc55c330b816273af94330b38277429
Jerud Moyer <jerudmoyer@Jeruds-Most-Excellent-Machine.local>
2025-12-30 16:54:45 -0800

    run it back again!

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -10,11 +10,3 @@ for env_file in ('.env', '.flaskenv'):
   env = os.path.join(os.getcwd(), env_file)
   if os.path.exists(env):
     load_dotenv(env)
-
-uri = os.getenv("DATABASE_URL")
-print('HERE IT IS!!!! ')
-print(uri)
-if uri.startswith("postgres://"):
-    print('DO WE EVEN GET IT HERE THOUGH???')
-    uri = uri.replace("postgres://", "postgresql://", 1)
-    print(uri)
